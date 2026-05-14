c7ff34ff4ced943ee17cef911c8df98a6e4c60ec
Simran. <simrankchoubey@gmail.com>
2026-05-14 16:13:53 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,13 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173" }));
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://edu-care-ai-self.vercel.app"
+  ],
+  credentials: true
+}))
 app.use(express.json());
 
 app.use("/api", aiRoutes)
